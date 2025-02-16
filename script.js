05785b71c3bdcceb66f990361ae269f384be2dba
Weslley Macedo Ribeiro <160357737+WeslleyMR@users.noreply.github.com>
2025-02-16 16:09:26 -0300

    script.js
    Deixei o código mais semântico.

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-let random1 = Math.floor(Math.random() * 6) + 1
+function rollDice(index) {
+    let random = Math.floor(Math.random() * 6) + 1;
+    let diceSource = 'img/dice' + random + '.png';
+    document.querySelectorAll('img')[index].setAttribute('src', diceSource);
+    return random;
+}
 
-let dicenumber = 'dice' + random1 + '.png' //dice(n).png
+let random1 = rollDice(0);
+let random2 = rollDice(1);
 
-let diceSoruce = 'img/' + dicenumber // img/dice1.png
-
-let getImage = document.querySelectorAll('img')[0]
-
-getImage.setAttribute('src', diceSoruce)
-
-
-let random2 = Math.floor(Math.random() * 6) + 1
-
-let dicenumber1 = 'dice' + random2 + '.png' //dice(n).png
-
-let diceSoruce1 = 'img/' + dicenumber1 // img/dice1.png
-
-let getImage1 = document.querySelectorAll('img')[1]
-
-getImage1.setAttribute('src', diceSoruce1)
-
-
-if(random1>random2){
-    document.querySelector('h1.result').innerHTML = '🏆 Jogador 1 ganhou!'
-}else if(random1 === random2){
-    document.querySelector('h1.result').innerHTML = '🤝 Jogo empatado'
-}else{
-    document.querySelector('h1.result').innerHTML = '🏆 Jogador 2 ganhou!'
+if (random1 > random2) {
+    document.querySelector('h1.result').innerHTML = '🏆 Jogador 1 ganhou!';
+} else if (random1 === random2) {
+    document.querySelector('h1.result').innerHTML = '🤝 Jogo empatado';
+} else {
+    document.querySelector('h1.result').innerHTML = '🏆 Jogador 2 ganhou!';
 }
